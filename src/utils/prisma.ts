381eacb7c0918f93386a1956b6bff6018961febf
EdsonATB <edsonaugusto2006@gmail.com>
2026-02-23 11:11:32 -0300

    add prisma.ts file

---
+++ src/utils/prisma.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from '../generated/prisma/client';
+
+const prismaClientSingleton = () => {
+  return new PrismaClient({
+    log: process.env.NODE_ENV === 'development' 
+      ? ['query', 'error', 'warn'] 
+      : ['error'],
+  } as any);
+};
+
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
+}
+
+const prisma = globalThis.prisma ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== 'production') {
+  globalThis.prisma = prisma;
+}
