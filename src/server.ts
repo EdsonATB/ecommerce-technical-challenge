b06bb75a2f8b2382c36bd3c64864b1ef2c1f0fe3
EdsonATB <edsonaugusto2006@gmail.com>
2026-03-18 14:23:46 -0300

    change default logs on server start

--- src/server.ts
+++ src/server.ts
@@ -16,7 +16,7 @@ async function startServer() {
         //iniciar server
         app.listen(PORT, () => {
             console.log(`Server running on port: ${PORT}`);
-            console.log(`API available at: http://localhost:${PORT}/api`);
+            console.log(`API endpoints available at: http://localhost:${PORT}/`);
             console.log(`Health check at: http://localhost:${PORT}/api/health`);
         });
     } catch (err) {
